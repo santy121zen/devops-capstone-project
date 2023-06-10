cb0ff59d56b69970d36c5717261d7cbeaad61abf
Santosh S <santosh@tattoo360.com>
2023-06-10 05:05:15 -0400

    Setup fix

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "****************************************"
 echo " Setting up Capstone Environment"
 echo "****************************************"
@@ -8,11 +9,10 @@ sudo add-apt-repository -y ppa:deadsnakes/ppa
 
 echo "Installing Python 3.9 and Virtual Environment"
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3.9 python3.9-venv
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3.8-venv
 
-echo "Making Python 3.9 the default..."
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
+echo "Making Python 3.8 the default..."
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 
 echo "Checking the Python version..."
 python3 --version
